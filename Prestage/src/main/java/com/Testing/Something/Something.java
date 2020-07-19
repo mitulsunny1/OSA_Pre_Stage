59a2cc6c9249a426213e4e71e1d681d7f5351a4a
shuvo <theking420620@gmail.com>
2020-07-19 01:56:50 -0400

    something

--- Prestage/src/main/java/com/Testing/Something/Something.java
+++ Prestage/src/main/java/com/Testing/Something/Something.java
@@ -3,6 +3,6 @@ package com.Testing.Something;
 public class Something {
 
 	public static void main(String[] args) {
-		System.out.println("The last stylebender");
+		System.out.println( "stylebender");
 	}
 }
