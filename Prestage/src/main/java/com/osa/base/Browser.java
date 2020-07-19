9bed9bf59719d89d2fb6f868aef77f181d736932
mitulsunny <41156630+mitulsunny@users.noreply.github.com>
2020-07-19 15:06:20 -0400

    adding Md's feature branch

--- Prestage/src/main/java/com/osa/base/Browser.java
+++ Prestage/src/main/java/com/osa/base/Browser.java
@@ -2,6 +2,26 @@ package com.osa.base;
 
 public class Browser {
  public static void main(String[] args) {
-	System.out.println("Opening browser");
+  //String n[]=getName();
+  String p[]= {"Tom","John","Raka","Chaka"};
+  printArrayValue(p);
+ }
+
+ 
+ 
+ //Returning String array
+ public static String[] getName() {
+	 String name[]= {"Md","Adnan","Anis","Khairul","Masum","Rupak","Shoshy"};
+	 return name;
+ }
+ 
+ //Parameterize method that is taking String array as a parameter.
+ public static void printArrayValue(String[] array) {
+	 for (String string : array) {
+		System.out.println(string);
 	}
+ }
+//Homework:
+ // Create array same as above for each of the data type like  int, double, shot,byte,long,char. 
+ 
 }
